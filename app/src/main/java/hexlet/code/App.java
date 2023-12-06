554cc7017b32cc1f05648ca41b40822a0fb632a5
piafson <pavorobyov@yandex.ru>
2023-12-06 15:05:26 +0800

    Step_4

--- app/src/main/java/hexlet/code/App.java
+++ app/src/main/java/hexlet/code/App.java
@@ -12,11 +12,27 @@ import com.zaxxer.hikari.HikariDataSource;
 
 import java.sql.SQLException;
 
+import gg.jte.ContentType;
+import gg.jte.TemplateEngine;
+import io.javalin.rendering.template.JavalinJte;
+import gg.jte.resolve.ResourceCodeResolver;
+
 @Slf4j
 public class App {
+    private static int getPort() {
+        String port = System.getenv().getOrDefault("PORT", "7070");
+        return Integer.valueOf(port);
+    }
+
+    private static String readResourceFile(String fileName) throws IOException {
+        var path = Paths.get("src", "main", "resources", fileName);
+        return Files.readString(path);
+    }
+
     public static Javalin getApp() throws IOException, SQLException {
         var hikariConfig = new HikariConfig();
         String jbcUrl = System.getenv("JDBC_DATABASE_URL");
+
         if (System.getenv("JDBC_DATABASE_URL") == null) {
             jbcUrl = "jdbc:h2:mem:piafson";
             hikariConfig.setUsername(System.getenv("JDBC_DATABASE_USERNAME"));
@@ -24,26 +40,35 @@ public class App {
         }
         hikariConfig.setJdbcUrl(jbcUrl);
 
+        var sql = readResourceFile("schema.sql");
         HikariDataSource dataSource = new HikariDataSource(hikariConfig);
 
-        var dbPath =  Paths.get("src", "main", "resources", "schema.sql");
-        var sql = Files.readString(dbPath);
-        var app = Javalin.create(config -> {
-            config.plugins.enableDevLogging();
-        });
+        log.info(sql);
         try (var connection = dataSource.getConnection();
-                var stmt = connection.createStatement()) {
+             var stmt = connection.createStatement()) {
             stmt.execute(sql);
         }
         BaseRepository.dataSource = dataSource;
 
-        app.get("/", ctx -> ctx.result("Hello World"));
+        var app = Javalin.create(config -> {
+            config.plugins.enableDevLogging();
+            JavalinJte.init(createTemplateEngine());
+        });
+
+        app.get("/", ctx -> ctx.render("index.jte"));
 
         return app;
     }
 
+    private static TemplateEngine createTemplateEngine() {
+        ClassLoader classLoader = App.class.getClassLoader();
+        ResourceCodeResolver codeResolver = new ResourceCodeResolver("templates", classLoader);
+        TemplateEngine templateEngine = TemplateEngine.create(codeResolver, ContentType.Html);
+        return templateEngine;
+    }
+
     public static void main(String[] args) throws IOException, SQLException {
         Javalin app = getApp();
-        app.start(7070);
+        app.start(getPort());
     }
 }
